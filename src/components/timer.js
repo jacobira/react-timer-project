322b739a3d1aa90f7cd0e9e4a003ed65f2b7173e
Jacob Baldwin <jacobbaldwin@Jacobs-MacBook-Pro.local>
2018-08-17 15:00:15 -0600

    Addition of JSX structuring inside of timer.js component.

---
+++ src/components/timer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+    render() {
+        return (
+            <div className="timer-box">
+                <div className="timer-content">
+                    <div className="timer-name">
+
+                    </div>
+                    <div className="timer-numbers">
+
+                    </div>
+                </div>
+                <div className="box-bottom">
+                    <div className="start-stop">
+
+                    </div>
+                    <div className="edit-delete">
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
